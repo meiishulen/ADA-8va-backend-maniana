a1116e0286404c7f09f0e1c9112f3920aceeb33c
HM <shadowdrone@hotmail.com>
2019-07-27 13:51:17 -0300

    Se sube la primer version de ejercicios en JAVA procedural. Son ejercicios basicos para familiarizarse con la sintaxis, entrada, salida de consola y sentencias de control(if, while, for, switch)

---
+++ Ejercicios/01_Procedural/Ejercicio41.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Ejercicio41: FREE
+ */
+public class Ejercicio41 {
+
+    public static Scanner Entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Ejercicios/01_Procedural/EjercicioXX.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * EjercicioXX
+ */
+public class EjercicioXX {
+
+    public static Scanner Entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
