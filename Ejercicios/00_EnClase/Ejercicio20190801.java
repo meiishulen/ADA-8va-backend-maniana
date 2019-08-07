9e14f332585c19e9bd632d86a5d781af5261e8e9
HM <shadowdrone@hotmail.com>
2019-08-07 10:39:45 -0300

    Se sube ejercicio Challenge 1 en java

--- Ejercicios/00_EnClase/Ejercicio20190801.java
+++ Ejercicios/00_EnClase/Ejercicio20190801.java
@@ -1,11 +1,47 @@
+import java.utils.*;
 /**
  * Ejercicio20190801
  */
 public class Ejercicio20190801 {
 
+    public static Sanner Teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        
-    
+        int n;
+        int[] nros;
+        int suma = 0;
+
+        System.out.println("Ingrese cant nros");
+
+        boolean hay13;
+
+        for(int i = 0; i < n; i++)
+        {
+            nros[i]=Teclado.nextInt();
+
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+           if ( nros[i] == 13)
+           {
+               hay13= true;
+           }
+           else {
+                if (!hay13)
+                {
+                 
+                    suma = suma + nros[i];
+                }
+                else
+                {
+                    hay13 = false;
+                }
+           }
+            
+        }
+
+        System.out.println("La suma es"+ suma);
     }
 }
